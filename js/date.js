3804fec8fcff69e606cc9e1c944578ea91b10f60
kavros <akavroulakis@gmail.com>
2019-02-26 21:20:27 +0000

    wip for graph section

--- js/date.js
+++ js/date.js
@@ -10,3 +10,12 @@ $(function() {
   		format: 'DD/MM/YYYY'
   	});
 });
+$(function() {
+
+	$('#resize').click(function(){
+      $('#axis1').removeClass("axis1 col-md-1 col-xs-1");	
+      $('#axis2').removeClass("axis2 col-md-1 col-xs-1");
+	  console.log("Hello world!"); 
+	   $('#mainGraph').removeClass("col-md-10 col-xs-10").addClass("col-md-12 col-xs-12");
+	});
+});
